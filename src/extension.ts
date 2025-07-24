708dc2dee1e577a1379f13f684a70f3bb63d18b9
topazrn <topazsorowako2@gmail.com>
2025-07-24 21:21:24 +0700

    Create a working extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import St from 'gi://St';
 
-export default class PlainExampleExtension extends Extension {
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+
+
+export default class ExampleExtension extends Extension {
+    _indicator: PanelMenu.Button | null = null;
+
     enable() {
+        // Create a panel button
+        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
+
+        // Add an icon
+        const icon = new St.Icon({
+            icon_name: 'face-laugh-symbolic',
+            style_class: 'system-status-icon',
+        });
+        this._indicator.add_child(icon);
+
+        // Add the indicator to the panel
+        Main.panel.addToStatusArea(this.uuid, this._indicator);
     }
 
     disable() {
+        this._indicator?.destroy();
+        this._indicator = null;
     }
 }
